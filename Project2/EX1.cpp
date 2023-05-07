9b80d550c14953dffdc15c154c8458da521f52f2
Геннадий <@DESKTOP-6NN51IE>
2023-05-07 12:37:19 +0300

    Добавьте файлы проекта.

---
+++ Project2/EX1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::endl;
+using std::cout;
+
+int main() {
+	cout << "Name       :  “The war and the peace”\n";
+	cout << "Avtor      :  L.N. Tolstoj\n";
+	cout << "Izdatelstvo:  Piter\n";
+	cout << "Pages      :  500.\n";
+	return 0;
+}
