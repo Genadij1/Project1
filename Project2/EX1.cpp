dfc25309d4df5ae5604a76c50d5660af2f72acc7
Gennadiy Andriyovych Byerlov <BXrlo_lk05@student.itstep.org>
2023-05-07 13:01:09 +0300

    Update EX1.cpp

--- Project2/EX1.cpp
+++ Project2/EX1.cpp
@@ -4,7 +4,7 @@ using std::endl;
 using std::cout;
 
 int main() {
-	cout << "Name       :  “The war and the peace”\n";
+	cout << "Name       :"<< "  \"The war and the peace\"\n";
 	cout << "Avtor      :  L.N. Tolstoj\n";
 	cout << "Izdatelstvo:  Piter\n";
 	cout << "Pages      :  500.\n";
